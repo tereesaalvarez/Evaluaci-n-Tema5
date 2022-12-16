d45824c22ffefee31d7be766c7865f4d3eab573f
tereesaalvarez <alvdeporteresa@gmail.com>
2022-12-16 18:07:47 +0100

    errores

--- operaciones.py
+++ operaciones.py
@@ -1,17 +1,28 @@
 def suma(a,b):
-    return a+b
+    if type(a) != int or type(b) != int:
+        raise TypeError("Error: Tipo de dato no válido")
+    else:
+        return a+b
 
 def resta(a,b):
-    return a-b
+    if type(a) != int or type(b) != int:
+        raise TypeError("Error: Tipo de dato no válido")
+    else:
+        return a-b
 
 def producto(a,b):
-    return a*b
+    if type(a) != int or type(b) != int:
+        raise TypeError("Error: Tipo de dato no válido")
+    else:
+        return a*b
 
 def division(a,b):
-    return a/b
-
-
-
+    if type(a) != int or type(b) != int:
+        raise TypeError("Error: Tipo de dato no válido")
+    elif b == 0:
+        raise ZeroDivisionError("Error: No es posible dividir entre cero")
+    else:
+        return a/b
 
 
 
