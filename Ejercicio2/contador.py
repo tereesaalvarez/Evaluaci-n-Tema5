4426c9eff65cc5bce538bc554b3ecbc763f9d7c0
tereesaalvarez <alvdeporteresa@gmail.com>
2022-12-26 12:35:53 +0100

    commit

--- Ejercicio2/contador.py
+++ Ejercicio2/contador.py
@@ -1,4 +1,19 @@
+import sys
 
-
-
+def contador():
+        try:
+            fichero = open("contador.txt", "r")
+            contador = int(fichero.read())
+            fichero.close()
+        except:
+            contador = 0
+        if len(sys.argv) == 2:
+            if sys.argv[1] == "inc":
+                contador += 1
+            elif sys.argv[1] == "dec":
+                contador -= 1
+        print(contador)    
+        fichero = open("contador.txt", "w")
+        fichero.write(str(contador))
+        fichero.close()
 
